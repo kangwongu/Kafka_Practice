453ba2e488a33e43c212d05e40b8e875c0423579
kangwongu <kwg0527@naver.com>
2024-02-17 11:57:06 +0900

    동시성 문제 해결 with Redis

--- example/api/src/main/kotlin/com/example/api/service/ApplyService.kt
+++ example/api/src/main/kotlin/com/example/api/service/ApplyService.kt
@@ -1,17 +1,21 @@
 package com.example.api.service
 
 import com.example.api.domain.Coupon
+import com.example.api.repository.CouponCountRepository
 import com.example.api.repository.CouponRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ApplyService(
     private val couponRepository: CouponRepository,
+    private val couponCountRepository: CouponCountRepository,
 ) {
 
     // 쿠폰 발급 로직
     fun apply(userId: Long) {
-        val count = couponRepository.count()
+//        val count = couponRepository.count()
+        // mysql말고 redis활용
+        val count = couponCountRepository.increment() ?: throw RuntimeException("Null 발생")
 
         if (count > 100) {
             return;
