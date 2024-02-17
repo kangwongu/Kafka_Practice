dc70f17257a99a4474bbeecbee67004b86d29d8b
kangwongu <kwg0527@naver.com>
2024-02-17 11:41:17 +0900

    동시성 문제 발생

--- example/api/src/test/kotlin/com/example/api/service/ApplyServiceTest.kt
+++ example/api/src/test/kotlin/com/example/api/service/ApplyServiceTest.kt
@@ -2,11 +2,14 @@ package com.example.api.service
 
 import com.example.api.repository.CouponRepository
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 @SpringBootTest
 class ApplyServiceTest @Autowired constructor(
@@ -24,6 +27,32 @@ class ApplyServiceTest @Autowired constructor(
         val result = couponRepository.count()
 
         // then
-        Assertions.assertThat(result).isEqualTo(1)
+        assertThat(result).isEqualTo(1)
+    }
+
+    @DisplayName("여러명 응모")
+    @Test
+    fun applyMulti() {
+        // given
+        val threadCount = 1000;     // 1000개의 요청
+        val executorService = Executors.newFixedThreadPool(32)
+        val latch = CountDownLatch(threadCount)
+
+        // when, 동시에 1000개의 쿠폰발급요청
+        for (i in 1 .. threadCount) {
+            val userId = i.toLong()
+            executorService.submit {
+                try {
+                applyService.apply(userId)
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+        latch.await()
+
+        // then
+        val result = couponRepository.count()
+        assertThat(result).isEqualTo(100)
     }
 }
